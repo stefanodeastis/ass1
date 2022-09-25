3c093937cd5e7ce50426149820df3c522841e4e9
stefanodeastis <stefanodeastis1967@gmail.com>
2022-09-25 13:51:53 +0200

    stesso programma del prof capito

--- prova_ass1.py
+++ prova_ass1.py
@@ -1,23 +1,36 @@
-#!/usr/bin/python3
-import sys
-import string
-import math
-''' questa è un'area di commento che uso per indicare cosa fa il programma o sorgente o script.
-    Il programma principale o il main, calcola l'area di un triangolo qualsiasi noti i suoi tre
-    lati usando la formula di Erone.
-    Formula  sqrt p*(p-a)*(p-b)*(p-c)    sqrt = radice quadrata, p = (a+b+c)/2   a,b,c lati
-    Salvatelo sul disco con il nome che vi propongo AreaTriangolo.py o come più vi piace
-'''
-a = int(input('inserisci il primo lato  '))
-print('primo lato = ', a)
-b = int(input('inserisci il secondo lato  '))
-print('secondo lato = ', b)
-c = int(input('inserisci il terzo lato  '))
-print('terzo lato = ',c)
-print("\n");
-print(" ora calcolo l'area di questo triangolo")
-print("\n")
-p = ((a+b+c))/2
-s = math.sqrt(p*(p-a)*(p-b)*(p-c))
+#!/usr/bin/env python3
+# Copyright (C) 2022 s.deastis@studenti.unipi.it
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-print("l'area  del triangolo è " , s)
+"""First assignment for the CMEPDA course, 2022/23.
+"""
+import argparse
+
+def process(file_path):
+ """
+ """
+ print(f'Opening input file {file_path}...')
+ with open(file_path, 'r') as input_file:
+  text = input_file.read()
+ print(text)
+ print('Done.')
+
+#https://www.programiz.com/python-programming/file-operation
+
+if __name__ == '__main__':
+ parser = argparse.ArgumentParser(description='Print some book statistics')
+ parser.add_argument('infile', type=str, help='path to the input file')
+ args = parser.parse_args()
+ process(args.infile)
