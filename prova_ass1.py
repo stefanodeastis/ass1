ec1bf892fca84640ee7787420e4208c48f145ec4
stefanodeastis <stefanodeastis1967@gmail.com>
2022-09-25 14:52:32 +0200

    aggiunte le pzioni read e histogram ma funzione principale non ancora creata

--- prova_ass1.py
+++ prova_ass1.py
@@ -17,20 +17,33 @@
 """First assignment for the CMEPDA course, 2022/23.
 """
 import argparse
+import time
 
-def process(file_path):
- """
- """
- print(f'Opening input file {file_path}...')
- with open(file_path, 'r') as input_file:
-  text = input_file.read()
- print(text)
- print('Done.')
+def read(file_path):
+    """read the book
+    """
+    print(f'Opening input file {file_path}...')
+    with open(file_path, 'r',encoding='utf-8') as input_file:
+        text = input_file.read()
+    print(text)
+    print('Done.')
+
+def plot_histogram():
+    """plot the histogram of relative frequences
+    """
 
 #https://www.programiz.com/python-programming/file-operation
 
 if __name__ == '__main__':
- parser = argparse.ArgumentParser(description='Print some book statistics')
- parser.add_argument('infile', type=str, help='path to the input file')
- args = parser.parse_args()
- process(args.infile)
+    start=time.time()
+    parser = argparse.ArgumentParser(description='Print some book statistics')
+    parser.add_argument('infile', type=str, help='path to the input file')
+    parser.add_argument('-r','--read', help='print the book', action='store_true')
+    parser.add_argument('-histo','--histogram', help='display a histogram of the frequences ', action='store_true')
+    args = parser.parse_args()
+    if args.read:
+        read(args.infile)
+    if args.histogram:
+        plot_histogram()
+    end=time.time()
+    print(end-start)
