ba794a20dca64f2b56bcf89f07e78e2850c6b973
stefanodeastis <stefanodeastis1967@gmail.com>
2022-09-26 21:45:14 +0200

    ultimi cambiamenti

--- prova_ass1.py
+++ prova_ass1.py
@@ -19,7 +19,6 @@
 import argparse
 import time
 import string
-from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -53,24 +52,60 @@ def relative_freq(book_text):
                 occurrency[j] +=1
                 break
     return occurrency
-#https://www.programiz.com/python-programming/file-operation
-if __name__ == '__main__':
-    '''programma principale
+
+def relative_freq_v2(book_text):
+    """compute the relative frequence of each letter of the alphabet with dict v2
+    """
+    occurrency=dict(zip(ALPHABET, np.zeros(len(ALPHABET))))
+
+    for letter in book_text:
+        if letter in occurrency:
+            occurrency[letter]+=1
+
+    return np.asarray(list(occurrency.values()))
+
+def count_line (book_text):
+    '''number of lines of the books
     '''
+    lines=1
+    for letter in book_text:
+        if letter == '\n':
+            lines+=1
+    return lines
+def count_words (book_text):
+    '''number of words of the books
+    '''
+    words=0
+    for i, letter in enumerate(book_text):
+        if letter not in ALPHABET and letter!= "'" and book_text[i-1] in ALPHABET:
+            words+=1
+    return words
+
+
+
+
+if __name__ == '__main__':
     start=time.time()
     parser = argparse.ArgumentParser(description='Print some book statistics')
     parser.add_argument('infile', type=str, help='path to the input file')
     parser.add_argument('-r','--read', help='print the book', action='store_true')
     parser.add_argument('-histo','--histogram', help='display the histogram', action='store_true')
+    parser.add_argument('-c','--char', help='number of characters', action='store_true')
+    parser.add_argument('-l','--lines', help='number of lines', action='store_true')
+    parser.add_argument('-w','--words', help='number of words', action='store_true')
     args = parser.parse_args()
     text=process(args.infile)
     if args.read:
         print(text)
-
-    relatives=relative_freq(text.lower())
-
+    text=text.lower()
+    relatives=relative_freq_v2(text)#serve lower
     print_relatives(relatives)
-
+    if args.char:
+        print(f'il numero di lettere presenti nel libro è:{relatives.sum()}')
+    if args.lines:
+        print(f'il numero di righe presenti nel file è:{count_line(text)}')
+    if args.words:
+        print(f'il numero di parole presenti è: {count_words(text)}')
     if args.histogram:
         plt.bar(ALPHABET,relatives)
     end=time.time()
